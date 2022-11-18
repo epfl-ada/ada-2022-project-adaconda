558b0a5dcb082d519fe370548ab1e50e6c7e5fef
Abiola Adeye <adeye.abiola@gmail.com>
2022-11-18 17:37:12 +0100

    created additional file to work on initial questions

---
+++ utils/constants.py
@@ -0,0 +1,49 @@
+BigMarkets = {
+    "LA": ["los angeles", "la", "lakers"],
+    "NY": ["knicks", "new york", "ny"],
+    "CH": ["bulls", "chicago"],
+    "PH": ["76ers", "seventy sixers", "seventysixers", "philadelphia", "phila"],
+    "DS": ["mavericks", "mavs", "dallas"],
+    "AT": ["hawks", "atlanta", "atl"],
+    "BO": ["celtics", "boston"],
+    "GSW": [
+        "gsw",
+        "golden state warriors",
+        "golden state",
+        "warriors",
+        "dubs",
+        "dubs nation",
+        "dubsnation",
+    ],
+}
+
+SmallMarkets = {
+    "ME": ["memphis", "grizzlies"],
+    "NO": ["pelicans", "pels", "neworleans", "new orleans", "nola"],
+    "OKC": ["okc", "oklahomacitythunder", "oklahoma"],
+    "MB": ["bucks", "milwaukee"],
+    "SA": ["spurs", "san antonio", "sanantonio"],
+    "UT": ["utah jazz", "utahjazz", "utah", "jazz"],
+    "IN": ["pacers", "indiana", "indi"],
+    "PT": ["trailblazers", "trail blazers", "blazers", "portland"],
+}
+
+
+Correspondence = {
+    "Chicago Bulls": "CH",
+    "Los Angeles Lakers": "LA",
+    "Memphis Grizzlies": "ME",
+    "Milwaukee Bucks": "MB",
+    "New Orleans Pelicans": "NO",
+    "New York Knicks": "NY",
+    "Oklahoma City Thunder": "OKC",
+    "Golden State Warriors": "GSW",
+    "Atlanta Hawks": "AT",
+    "Boston Celtics": "BO",
+    "Dallas Mavericks": "DS",
+    "Indiana Pacers": "IN",
+    "Philadelphia 76ers": "PH",
+    "San Antonio Spurs": "SA",
+    "Portland Trail Blazers": "PT",
+    "Utah Jazz": "UT",
+}
